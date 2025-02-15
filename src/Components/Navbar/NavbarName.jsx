e0dc5fe5125ed67a4e83152aba6e247bc09bf17c
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-15 09:53:34 -0800

    added scrabble pieces to my name :)

--- src/Components/Navbar/NavbarName.jsx
+++ src/Components/Navbar/NavbarName.jsx
@@ -9,19 +9,93 @@ const NavbarName = () => {
         flexGrow: 1,
         display: "flex",
         direction: "row",
+        paddingTop: '5px'
       }}
     >
-      <ScrabblePiece letter="B" height="30px" width="30px" padding=".25rem" fontSize="15x" fontWeight='bold' />
+      <ScrabblePiece
+        letter="B"
+        height="25px"
+        width="25px"
+        padding=".20rem"
+        fontSize="15x"
+        fontWeight="bold"
+      />
 
       <Typography
         sx={{
           color: "black",
           whiteSpace: "nowrap",
           fontWeight: "550",
-          fontSize: "17px",
+          fontSize: "18px",
+          paddingTop: "4px",
+          paddingLeft: "1.5px",
+          paddingRight: "3px",
         }}
       >
-        e-Asia McKerracher
+        e-
+      </Typography>
+      <ScrabblePiece
+        letter="A"
+        height="25px"
+        width="25px"
+        padding=".20rem"
+        fontSize="15x"
+        fontWeight="bold"
+      />
+      <Typography
+        sx={{
+          color: "black",
+          whiteSpace: "nowrap",
+          fontWeight: "550",
+          fontSize: "18px",
+          paddingTop: "4px",
+          paddingLeft: "1.5px",
+          paddingRight: "23px",
+        }}
+      >
+        sia
+      </Typography>
+      <ScrabblePiece
+        letter="M"
+        height="25px"
+        width="25px"
+        padding=".20rem"
+        fontSize="15x"
+        fontWeight="bold"
+      />
+      <Typography
+        sx={{
+          color: "black",
+          whiteSpace: "nowrap",
+          fontWeight: "550",
+          fontSize: "18px",
+          paddingTop: "4px",
+          paddingLeft: "1.5px",
+          paddingRight: "3px",
+        }}
+      >
+        c
+      </Typography>
+      <ScrabblePiece
+        letter="K"
+        height="25px"
+        width="25px"
+        padding=".20rem"
+        fontSize="15x"
+        fontWeight="bold"
+      />
+      <Typography
+        sx={{
+          color: "black",
+          whiteSpace: "nowrap",
+          fontWeight: "550",
+          fontSize: "18px",
+          paddingTop: "4px",
+          paddingLeft: "1.5px",
+          paddingRight: "3px",
+        }}
+      >
+        erracher
       </Typography>
     </Box>
   );
