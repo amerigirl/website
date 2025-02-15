60086cbebbf997eaecce78d76455c0069153afa7
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-14 22:56:31 -0800

    done with initial, hard-coded scrabble piece component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import Navbar from "./Components/Navbar";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import AboutMe from "./Components/AboutMe";
 import SkillsCard from "./Components/Files/SkillsCard";
+import { amber } from "@mui/material/colors";
 
 const theme = createTheme({
   palette: {
     primary: {
       main: "#008080",
+    },
+    secondary: {
+      main: amber[200],
     },
   },
   components: {
--- src/Components/ScrabblePiece.jsx
+++ src/Components/ScrabblePiece.jsx
@@ -4,27 +4,34 @@ import React from "react";
 const ScrabblePiece = () => {
   return (
     //1. create a small box
-    //2. add the right background--where is it? wood, grainy
+    //2. add the right background--where is it? wood,
     //   -raising the sides/css, maybe outline in black
+    //3. create the letter and move it to the center of the box
 
-    <Box sx={{
-        height: '70px',
-        width: '70px',
-        backgroundColor: 'pink', 
-     
+    //4. create a sub number and move it to the bottom right of the box
 
-    }}>
-      <Typography sx={{
-        textAlign: 'center',
-        padding: '1.5rem'
-
-      }}>S</Typography>
+    <Box
+      sx={{
+        height: "60px",
+        width: "60px",
+        backgroundColor: "secondary.main",
+        border: "2px solid black",
+        boxShadow: "0,0,0,.3",
+      }}
+    >
+      <Typography
+        sx={{
+          textAlign: "center",
+          padding: ".7rem",
+          fontSize: "1.6rem",
+        }}
+      >
+        S
+      </Typography>
     </Box>
   );
 
-  //3. create the letter and move it to the center of the box
 
-  //4. create a sub number and move it to the bottom right of the box
 };
 
 export default ScrabblePiece;
