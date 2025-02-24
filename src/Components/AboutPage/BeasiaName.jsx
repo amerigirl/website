19c4c557b186875f5ae626bf4874c91c8d146e9b
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-02-23 17:01:25 -0800

    working with name and image

--- src/Components/AboutPage/BeasiaName.jsx
+++ src/Components/AboutPage/BeasiaName.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+
 const BeasiaName = () => {
   const name = "BEASIA";
 
@@ -8,15 +9,33 @@ const BeasiaName = () => {
       sx={{
         display: "flex",
         justifyContent: "space-evenly",
+        alignItems: "center",
+        marginTop: "12rem",
       }}
     >
       <Box sx={{ display: "grid" }}>
         {name.split("").map((char, index) => (
-          <Typography key={index}>{char}</Typography>
+          <Typography
+            key={index}
+            sx={{
+              fontSize: "3rem",
+              fontFamily: "fantasy",
+              color: "primary.light",
+              fontStyle: "oblique",
+            }}
+          >
+            {char}
+          </Typography>
         ))}
       </Box>
+
       <Box>
-        <img src="./meAndGrogu.jpg"/>
+        <img
+          src="./meAndGrogu.jpg"
+          alt="BeAsia and Grogu Doll"
+          height="375vh"
+          width={"320vw"}
+        />
       </Box>
     </Box>
   );
