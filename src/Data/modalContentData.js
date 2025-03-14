418ef2f252188f0fcd80e2f142192da5fce9b002
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-03-13 22:04:15 -0700

    final touches on modal

--- src/Data/modalContentData.js
+++ src/Data/modalContentData.js
@@ -4,15 +4,15 @@ const timelineItems = [
     content: "Truman State University (2009)",
     color: "grey",
     modalContent:
-      "A great college in the heart of the Midwest! I started my learning journey here, graduating with both a Bachelor of Arts and a Master's of Arts in English. Click here to learn more about Truman State University, it's a great place to grow and learn!",
+      "A great college in the heart of the Midwest! I started my learning journey here, graduating with both a Bachelor of Arts and a Master's of Arts in English. Truman State University is a great place to grow and learn!",
     modalImage: "../TrumanState.jpg",
     visitSite: "https://www.truman.edu/",
     visitTitle: "Visit Truman",
   },
-    {
+  {
     content: "LaunchCode (2022)",
     color: "primary",
-    modalContent: 
+    modalContent:
       "LaunchCode was instrumental in my journey from Educator to Tech Professional. This 9-month program taught the basics of frontend (JavaScript, HTML/CSS, Bootstrap) and backend (mySQL, SQL). Taught by current professionals, I am forever grateful for the wisdom and the early-entry guidance I needed to be successful.",
     modalImage: "/launchCode.jpg",
     visitSite: "https://www.launchcode.org/",
@@ -22,13 +22,17 @@ const timelineItems = [
     content: "Microsoft AZ-900 (2023)",
     color: "secondary",
     modalContent:
-      "Microsoft's AZ-900 was the first professional certification in my career. One great thing certification did was remind me that with effort, time, and practice, you can learn anything. You can learn more about the AZ-900 here.",
+      "Microsoft's AZ-900 was the first professional certification in my career. One great thing certification did was remind me that with effort, time, and practice, you can learn anything.",
+    modalImage: "/AZ900.png",
+    visitSite:
+      "https://learn.microsoft.com/en-us/credentials/certifications/azure-fundamentals/?practice-assessment-type=certification",
+    visitTitle: "Visit AZ-900",
   },
   {
     content: "Renton Technical College (2026)",
     color: "grey",
     modalContent:
-      "So excited to be learning and growing (AGAIN) at RTC!  This was the college I went to shortly before transferring to Truman State.  Returning home to the PNW and learning new skills in the tech industry has been amazing.  You can learn more about RTC here.",
+      "So excited to be learning and growing (AGAIN) at RTC!  This was the college I went to shortly before transferring to Truman State.  Returning home to the PNW and learning new skills in the tech industry has been amazing!",
     modalImage: "/rentonTech.webp",
     visitSite: "https://www.rtc.edu/",
     visitTitle: "Visit RTC",
