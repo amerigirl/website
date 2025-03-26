bac334074c3356fcb22cdc38d996f97fe2d872e6
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-03-26 15:09:44 -0700

    small tweaks before starting skill bar

---
+++ src/Components/AboutPage/MAJTitle.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box, ThemeProvider } from "@mui/material";
+import Navbar from "../Navbar/Navbar";
+import theme from "../../theme";
+import About from "./About";
+import MyAcademicJourney from "./MyAcademicJourney/Timeline";
+import SectionTitle from "../SectionTitle";
+import MouseOutlinedIcon from "@mui/icons-material/MouseOutlined";
+
+const MAJTitle = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <About />
+
+      <Box sx={{ marginTop: "17rem", marginBottom: "10rem" }}>
+        <SectionTitle
+          title={"My Academic Journey"}
+          subtitle={
+            <>
+              Click the text below to learn more :)
+              <MouseOutlinedIcon
+                sx={{
+                  fontSize: "2.5rem",
+                  verticalAlign: "middle",
+                  color: "#0dac1b",
+                  paddingLeft: ".5rem",
+                }}
+              />
+            </>
+          }
+        />
+      </Box>
+      <MyAcademicJourney />
+    </ThemeProvider>
+  );
+};
+
+export default MAJTitle;
--- src/Components/SkillsPage/SkillsPage.jsx
+++ src/Components/SkillsPage/SkillsPage.jsx
@@ -3,7 +3,7 @@ import theme from "../../theme";
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
-import { Container, Typography, Box } from "@mui/material";
+import { Box } from "@mui/material";
 import SectionTitle from "../SectionTitle";
 
 const SkillsPage = () => {
