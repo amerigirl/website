0f29f1b8d64b9ce96c094aae295fe20e6b400a38
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-04-14 19:15:00 -0700

    deleted unneeded commented code

--- src/Components/SkillsPage/SkillsPage.jsx
+++ src/Components/SkillsPage/SkillsPage.jsx
@@ -40,32 +40,3 @@ const SkillsPage = () => {
 
 export default SkillsPage;
 
-// <Container
-//       sx={{
-//         display: "flex",
-//         alignItems: "center",
-//         gap: "2rem",
-//         marginTop: "10rem",
-//       }}
-//     >
-//       <Box>
-//         <img
-//           src="../../../frontEndWork.jpg"
-//           alt="frontend work"
-//           style={{
-//             height: "30vh",
-//             outline: "3px solid black",
-//             borderRadius: "15px",
-//           }}
-//         />
-//       </Box>
-//       <Box>
-//         <Typography
-//           sx={{
-//             fontSize: "20px",
-//           }}
-//         >
-//           Most days, you will find me right here at my desk, tinkering with a project, reading technical docs, or watching a youtube video about a React. Most days, you will find me right here at my desk, tinkering with a project, reading technical docs, or watching a youtube video about a React.Most days, you will find me right here at my desk, tinkering with a project, reading technical docs, or watching a youtube video about a React.Most days, you will find me right here at my desk, tinkering with a project, reading technical docs, or watching a youtube video about a React.
-//         </Typography>
-//       </Box>
-//     </Container>
