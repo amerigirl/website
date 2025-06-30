5c3b2982f973e68706089683055146d4c6f70b16
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-06-30 12:08:31 -0700

    adding footer router

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,6 +9,7 @@ import {
   Typography,
 } from "@mui/material";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { Link } from "react-router-dom";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   height: "10rem",
@@ -17,11 +18,39 @@ const Footer = () => {
   return (
     <Box>
       <AppBar position="static" sx={{ top: "auto", bottom: 0 }}>
-        <StyledToolbar>
+        <StyledToolbar
+          sx={{ display: "flex", justifyContent: "space-between" }}
+        >
           <Box>
             <Typography sx={{ fontSize: "2rem", fontWeight: "bold" }}>
               Be-Asia McKerracher
             </Typography>
+          </Box>
+
+          <Box>
+            <Box>
+              <ArrowUpwardIcon style={{ fontSize: "2rem" }} />
+            </Box>
+            <Box
+              sx={{
+                color: "white",
+              }}
+            >
+              <Button
+                id="home"
+                color="inherit"
+                component={Link}
+                to="/"
+                sx={{
+                  minWidth: 0,
+                  paddingLeft: 0,
+                  paddingRight: 0,
+                  justifyContent: "center",
+                }}
+              >
+                Home
+              </Button>
+            </Box>
           </Box>
         </StyledToolbar>
       </AppBar>
