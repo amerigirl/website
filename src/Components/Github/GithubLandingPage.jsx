8610e69c07b585e2975af223c4138138e689f787
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-07-02 20:50:47 -0700

    filling out projects and future learnings page

--- src/Components/Github/GithubLandingPage.jsx
+++ src/Components/Github/GithubLandingPage.jsx
@@ -4,21 +4,35 @@ import Navbar from "../Navbar/Navbar";
 import theme from "../../theme";
 import Footer from "../Footer/Footer";
 
- 
-
 const GithubLandingPage = () => {
   return (
-      <ThemeProvider theme={theme}>
-          <Navbar  />
-          <Box sx={{ marginTop: "8rem", textAlign: "center"}}>
-              <Typography sx={{fontSize: "2.5rem", fontWeight: "bold"}}>
-                  Projects and Future Learnings
-              </Typography>
-          </Box>
-          <Box sx={{mt: 4, textAlign: "center", p:4}}>
-              <img src="./contributions.png" style={{borderRadius:"16px"}}/>
-          </Box>
-          <Footer />
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <Box sx={{ marginTop: "8rem", textAlign: "center" }}>
+        <Typography sx={{ fontSize: "2.5rem", fontWeight: "bold" }}>
+          Projects and Future Learnings
+        </Typography>
+      </Box>
+      <Box sx={{ mt: 4, textAlign: "center", p: 4 }}>
+        <img src="./contributions.png" style={{ borderRadius: "16px" }} />
+      </Box>
+      <Box
+        sx={{
+          maxWidth: "800px",
+          margin: "0 auto 2rem",
+          textAlign: "center",
+          p: 2,
+        }}
+      >
+        <Typography style={{ fontSize: "1.2rem" }}>
+          Projects are how we learn and grow! My Github (like most of ours) is
+          littered with projects that I've started and not quite finished, old
+          class assignments, and general practice. But inside that Github
+          profile is also a few solid, complete jewels that I'd love to share!
+        </Typography>
+      </Box>
+
+      <Footer />
     </ThemeProvider>
   );
 };
