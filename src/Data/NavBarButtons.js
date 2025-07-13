1b3b271631248399e629e88c09d6d74191b14c7b
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-07-12 20:38:32 -0700

    navBarButton data complete

--- src/Data/NavBarButtons.js
+++ src/Data/NavBarButtons.js
@@ -1,7 +1,27 @@
 navBarButtons = [
-    {
-        name: "Home",
-        link: "/",
-        component: Link,
-    }
-]
+  {
+    name: "Home",
+    link: "/",
+    component: Link,
+  },
+  {
+    name: "About",
+    link: "/aboutPage",
+    component: Link,
+  },
+  {
+    name: "Skills",
+    link: "/skillsPage",
+    component: Link,
+  },
+  {
+    name: "Projects",
+    link: "/projects",
+    component: Link,
+  },
+  {
+    name: "Contact",
+    link: "#contact",
+    component: HashLink,
+  },
+];
