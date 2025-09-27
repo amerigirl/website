8e71a3483e51ae0a6bd07c9672a0241dd8b4ed3b
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-09-26 22:41:02 -0700

    creating contacMeDetailsTemplate

---
+++ src/Components/LandingPage/ContactForm/ContactMeDetailsTemplate.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PhoneOutlinedIcon from "@mui/icons-material/PhoneOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import theme from "../../../theme";
+
+const ContactMeDetailsTemplate = () => {
+  return (
+    <Box display={"grid"} gap={"2.5rem"}>
+      <EmailOutlinedIcon sx={{ fontSize: 40, color: "primary.main" }} />
+      <Typography variant="h4" component={"div"} fontSize={"1.9rem"}>
+        {children}
+      </Typography>
+      <Typography
+        variant="h5"
+        component={"div"}
+        fontSize={"1.6rem"}
+        marginLeft={"4.5rem"}
+        color="grey"
+      >
+        {children}
+      </Typography>
+    </Box>
+  );
+};
+export default ContactMeDetailsTemplate;
