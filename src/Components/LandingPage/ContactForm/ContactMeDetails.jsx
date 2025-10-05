30272eacdfed326f6831937c762661542d537abd
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-10-04 20:22:06 -0700

    adjusting icon image setup

--- src/Components/LandingPage/ContactForm/ContactMeDetails.jsx
+++ src/Components/LandingPage/ContactForm/ContactMeDetails.jsx
@@ -11,45 +11,48 @@ const SocialMediaContact = () => {
   return (
     <>
       <Box>
-          {
-          ContactMeDetailsTemplateData.map((data, index) => {
-            console.log(data);
-            return <ContactMeDetailsTemplate key={index} info={data} />;
-          })
-        } 
+        {ContactMeDetailsTemplateData.map((data, index) => {
+          console.log(data);
+          return <ContactMeDetailsTemplate key={index} info={data} />;
+        })}
       </Box>
- 
+      <Box sx={{display: "flex"}}>
+        <Box>
+          <a
+            href="https://www.linkedin.com/in/be-asia/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Box
+              component="img"
+              sx={{
+                height: 70,
+                width: 70,
+              }}
+              alt="LinkedIn Icon"
+              src="../../linkedInLogo.jpg"
+            />
+          </a>
+        </Box>
 
-      <a
-        href="https://www.linkedin.com/in/be-asia/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Box
-          component="img"
-          sx={{
-            height: 70,
-            width: 70,
-          }}
-          alt="LinkedIn Icon"
-          src="../../linkedInLogo.jpg"
-        />
-      </a>
-      <a
-        href="https://github.com/amerigirl"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Box
-          component="img"
-          sx={{
-            height: 45,
-            width: 45,
-          }}
-          alt="Github Icon"
-          src="../../github.png"
-        />
-      </a>
+        <Box>
+          <a
+            href="https://github.com/amerigirl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Box
+              component="img"
+              sx={{
+                height: 45,
+                width: 45,
+              }}
+              alt="Github Icon"
+              src="../../github.png"
+            />
+          </a>
+        </Box>
+      </Box>
     </>
   );
 };
